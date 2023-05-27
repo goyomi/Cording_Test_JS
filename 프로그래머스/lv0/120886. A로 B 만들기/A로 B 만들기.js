d8fe175ff236897b2f8fa5e1945b802babf8314c
goyomi <122963246+goyomi@users.noreply.github.com>
2023-05-28 07:09:34 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,7 +1,5 @@
 function solution(before, after) {
     const splBefore = before.split('').sort().join('')
     const splAfter = after.split('').sort().join('')
-    if(splBefore===splAfter){
-        return 1;
-    } else return 0;
+    return splBefore===splAfter ? 1: 0;
 }
