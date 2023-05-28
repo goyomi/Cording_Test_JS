0a9dd92b17dbff51202624f401538381cea86881
goyomi <122963246+goyomi@users.noreply.github.com>
2023-05-28 09:20:40 +0900

    [level 0] Title: 모스부호 (1), Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -1,21 +1,18 @@
 function solution(letter) {
-  const morse = {
-    '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
-    '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
-    '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
-    '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
-    '-.--': 'y', '--..': 'z'
-  };
-
-  const morseLetters = letter.split(' ');
-  let result = '';
-
-  for (let i = 0; i < morseLetters.length; i++) {
-    const morseLetter = morseLetters[i];
-    if (morseLetter in morse) {
-      result += morse[morseLetter];
+    var answer = '';
+    const morseLetter = letter.split(' ');
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+};
+    for(let i=0; i<morseLetter.length; i++){
+        const morseVal = morseLetter[i]
+        if(morseVal in morse){
+            answer+=morse[morseVal];
+        }
     }
-  }
-
-  return result;
+    return answer;
 }
