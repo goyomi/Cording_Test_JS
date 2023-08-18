32176ea704a9fbbd2055b1852bb21deb34dfbefc
goyomi <122963246+goyomi@users.noreply.github.com>
2023-08-19 00:02:39 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    let even = 0;
+    let odd = 0;
+    
+    for(let i=0; i<num_list.length; i++){
+        (i%2===0)?even+=num_list[i]:odd+=num_list[i];
+    }
+    return Math.max(even, odd);
+}
