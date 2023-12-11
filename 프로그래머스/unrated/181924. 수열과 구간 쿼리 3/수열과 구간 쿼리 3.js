71948440d8636ed0e111c91af837afb576511bb9
goyomi <122963246+goyomi@users.noreply.github.com>
2023-12-11 15:30:50 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.19 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,10 @@
+function solution(arr, queries) {
+    
+    for(const query of queries){
+        let temp = arr[query[0]];
+        arr[query[0]] = arr[query[1]];
+        arr[query[1]] = temp;
+    }
+    
+    return arr;
+}
