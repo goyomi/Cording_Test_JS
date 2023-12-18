0db273eb4536a76a46041e177beea822d6c0dfee
goyomi <122963246+goyomi@users.noreply.github.com>
2023-12-18 21:29:20 +0900

    [level unrated] Title: qr code, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -0,0 +1,11 @@
+function solution(q, r, code) {
+    let str = '';
+    
+    for(let i=0; i<code.length; i++){
+        if(i % q === r){
+            str += code[i]
+        }
+    }
+    
+    return str;
+}
