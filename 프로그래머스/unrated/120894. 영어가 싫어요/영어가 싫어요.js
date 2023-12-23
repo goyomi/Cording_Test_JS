04b7d005f56db8b91f9d98aa2b98e850eff316e7
goyomi <122963246+goyomi@users.noreply.github.com>
2023-12-23 22:32:13 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    const enNumbers = {"zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, 
+                       "six":6, "seven":7, "eight":8, "nine":9 };
+    
+    for(let key in enNumbers){
+        const regexp = new RegExp(key, "g");
+        numbers = numbers.replace(regexp, enNumbers[key]);
+    }
+    
+    return parseInt(numbers);
+}
