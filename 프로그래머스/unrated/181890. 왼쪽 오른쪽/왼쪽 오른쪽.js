ff2c94d9a66f3bd2cfd7469f0ffdda6196882aab
goyomi <122963246+goyomi@users.noreply.github.com>
2023-12-24 15:40:10 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,13 @@
+function solution(str_list) {
+    const indexL = str_list.indexOf('l');
+    const indexR = str_list.indexOf('r');
+    
+    if(indexL !== -1 && (indexL < indexR || indexR === -1)){
+        return str_list.slice(0, indexL);
+    }else if(indexR !== -1 && (indexR < indexL || indexL === -1)){
+        return str_list.slice(indexR + 1);
+    }else {
+        return [];
+    }
+
+}
