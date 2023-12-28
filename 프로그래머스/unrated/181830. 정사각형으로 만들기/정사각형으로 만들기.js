18a56dabf5a9c4b4159aede0d683cbec05e52c85
goyomi <122963246+goyomi@users.noreply.github.com>
2023-12-28 12:08:15 +0900

    [level unrated] Title: 정사각형으로 만들기, Time: 0.07 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,14 +1,16 @@
 function solution(arr) {
+    const rows = arr.length; // 행
+    const cols = arr[0].length; // 열
     
-    for(let i=0; i<arr.length; i++){
-        if(arr.length > arr[i].length){
-            while(arr.length !== arr[i].length){
-                arr[i].push(0);
+    if(rows > cols){
+        arr.forEach(row => {
+            while(row.length < rows){
+                row.push(0);
             }
-        }else{
-            while(arr.length !== arr[i].length){
-                arr.push([]);
-            }
+        });
+    }else if(cols > rows){
+        while(arr.length < cols){
+            arr.push(new Array(cols).fill(0));
         }
     }
 
