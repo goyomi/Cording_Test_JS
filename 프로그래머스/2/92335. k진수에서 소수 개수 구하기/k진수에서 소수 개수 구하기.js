6fbe1232c37bb653b2a7016c8a7d58ab1f470ff7
goyomi <122963246+goyomi@users.noreply.github.com>
2024-02-10 17:00:28 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,13 @@
+function solution(n, k) {
+    const isPrime = num => {
+        if(num < 2) return false;
+        if(num === 2) return true;
+        if(num % 2 === 0) return false;
+        for(let i=3; i<=Math.sqrt(num); i+= 2){
+            if(num % i === 0) return false;
+        }
+        return true;
+    };
+    
+    return n.toString(k).split(0).filter(num => num && isPrime(parseInt(num, 10))).length;
+}
